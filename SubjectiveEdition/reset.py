150db7105b57adb3fd227978296404aee23ad414
ZenSky123 <zensky@126.com>
2018-06-25 10:39:13 +0800

    make reset be a method

--- SubjectiveEdition/reset.py
+++ SubjectiveEdition/reset.py
@@ -2,13 +2,19 @@ import configparser
 import json
 import codecs
 
-cfg = configparser.ConfigParser()
-cfg.read('config.ini')
-data_filename = cfg.get('path', 'filename')
 
-data = json.load(codecs.open(data_filename, 'r', 'utf-8'))
+def reset():
+    cfg = configparser.ConfigParser()
+    cfg.read('config.ini')
+    data_filename = cfg.get('path', 'filename')
 
-for d in data:
-    d["count"] = 0
+    data = json.load(codecs.open(data_filename, 'r', 'utf-8'))
 
-json.dump(data, codecs.open(data_filename, 'w', 'utf-8'), ensure_ascii=False, indent=4)
+    for d in data:
+        d["count"] = 0
+
+    json.dump(data, codecs.open(data_filename, 'w', 'utf-8'), ensure_ascii=False, indent=4)
+
+
+if __name__ == '__main__':
+    reset()
